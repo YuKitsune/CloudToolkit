c89b15c5366ec03961e9be781da842023b0d28db
Eoin Motherway <eoinmoth@yahoo.ie>
2020-09-06 22:37:38 +1000

    Stub projects for Object Storage

---
+++ src/CloudToolkit/CloudToolkit.ObjectStorage/IObjectStorageService.cs
@@ -0,0 +1,9 @@
+﻿namespace CloudToolkit.ObjectStorage
+{
+    /// <summary>
+    ///     The interface representing a class capable of interacting with an Object Storage service.
+    /// </summary>
+    public interface IObjectStorageService
+    {
+    }
+}
