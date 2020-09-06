108425ca01e6e3fdcbf057f76100b8faaf439533
Eoin Motherway <eoinmoth@yahoo.ie>
2020-09-06 21:18:34 +1000

    Added email abstractions

---
+++ src/CloudToolkit/CloudToolkit.Email/BaseEmailSender.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Net.Mail;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CloudToolkit.Email
+{
+	/// <summary>
+	///		The base implementation of the <see cref="IEmailSender"/>.
+	/// </summary>
+    public abstract class BaseEmailSender : IEmailSender
+	{
+		/// <summary>
+		///		Gets the default "From" address to use when sending emails.
+		/// </summary>
+		public string DefaultFromAddress { get; }
+
+		/// <summary>
+		///		Gets the default display name to use when sending emails.
+		/// </summary>
+		public string DefaultDisplayName { get; }
+
+		protected BaseEmailSender(string defaultFromAddress, string defaultDisplayName = null)
+		{
+			if (string.IsNullOrWhiteSpace(defaultFromAddress)) throw new ArgumentNullException(nameof(defaultFromAddress));
+			DefaultFromAddress = defaultFromAddress;
+			DefaultDisplayName = defaultDisplayName;
+		}
+
+		/// <summary>
+		///     Sends a plain-text email to the <paramref name="recipientAddress"/>.
+		/// </summary>
+		/// <param name="recipientAddress">
+		///     The email address of the recipient.
+		/// </param>
+		/// <param name="subject">
+		///     The subject of the email.
+		/// </param>
+		/// <param name="message">
+		///     The message to send.
+		/// </param>
+		public virtual void SendEmail(string recipientAddress, string subject, string message) =>
+			SendEmail(new MailAddress(DefaultFromAddress, DefaultDisplayName), recipientAddress, subject, message);
+
+		/// <summary>
+		///     Sends a plain-text email to the <paramref name="recipientAddress"/>.
+		/// </summary>
+		/// <param name="fromAddress">
+		///		The <see cref="MailAddress"/> to send the email from.
+		/// </param>
+		/// <param name="recipientAddress">
+		///     The email address of the recipient.
+		/// </param>
+		/// <param name="subject">
+		///     The subject of the email.
+		/// </param>
+		/// <param name="message">
+		///     The message to send.
+		/// </param>
+		public virtual void SendEmail(
+			MailAddress fromAddress,
+			string recipientAddress,
+			string subject,
+			string message) =>
+			SendEmailAsync(fromAddress, recipientAddress, subject, message).GetAwaiter().GetResult();
+
+		/// <summary>
+		///     Sends a plain-text email to the <paramref name="recipientAddress"/> as an asynchronous operation.
+		/// </summary>
+		/// <param name="recipientAddress">
+		///     The email address of the recipient.
+		/// </param>
+		/// <param name="subject">
+		///     The subject of the email.
+		/// </param>
+		/// <param name="message">
+		///     The message to send.
+		/// </param>
+		/// <param name="cancellationToken">
+		///		The <see cref="CancellationToken"/> to observe.
+		/// </param>
+		/// <returns>
+		///     The <see cref="Task"/> representing the asynchronous operation.
+		/// </returns>
+		public virtual async Task SendEmailAsync(
+			string recipientAddress,
+			string subject,
+			string message,
+			CancellationToken cancellationToken = default) => 
+			await SendEmailAsync(
+				new MailAddress(DefaultFromAddress, DefaultDisplayName),
+				recipientAddress,
+				subject,
+				message,
+				cancellationToken);
+
+		/// <summary>
+		///     Sends a plain-text email to the <paramref name="recipientAddress"/> as an asynchronous operation.
+		/// </summary>
+		/// <param name="fromAddress">
+		///		The <see cref="MailAddress"/> to send the email from.
+		/// </param>
+		/// <param name="recipientAddress">
+		///     The email address of the recipient.
+		/// </param>
+		/// <param name="subject">
+		///     The subject of the email.
+		/// </param>
+		/// <param name="message">
+		///     The message to send.
+		/// </param>
+		/// <param name="cancellationToken">
+		///		The <see cref="CancellationToken"/> to observe.
+		/// </param>
+		/// <returns>
+		///     The <see cref="Task"/> representing the asynchronous operation.
+		/// </returns>
+		public abstract Task SendEmailAsync(
+			MailAddress fromAddress,
+			string recipientAddress,
+			string subject,
+			string message,
+			CancellationToken cancellationToken = default);
+	}
+}
---
+++ src/CloudToolkit/CloudToolkit.Email/IEmailSender.cs
@@ -0,0 +1,95 @@
+﻿using System.Net.Mail;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CloudToolkit.Email
+{
+	/// <summary>
+	///		An interface representing a class capable of sending emails.
+	/// </summary>
+    public interface IEmailSender
+	{
+		/// <summary>
+		///     Sends a plain-text email to the <paramref name="recipientAddress"/>.
+		/// </summary>
+		/// <param name="recipientAddress">
+		///     The email address of the recipient.
+		/// </param>
+		/// <param name="subject">
+		///     The subject of the email.
+		/// </param>
+		/// <param name="message">
+		///     The message to send.
+		/// </param>
+		void SendEmail(string recipientAddress, string subject, string message);
+		
+		/// <summary>
+		///     Sends a plain-text email to the <paramref name="recipientAddress"/>.
+		/// </summary>
+		/// <param name="fromAddress">
+		///		The <see cref="MailAddress"/> to send the email from.
+		/// </param>
+		/// <param name="recipientAddress">
+		///     The email address of the recipient.
+		/// </param>
+		/// <param name="subject">
+		///     The subject of the email.
+		/// </param>
+		/// <param name="message">
+		///     The message to send.
+		/// </param>
+		void SendEmail(MailAddress fromAddress, string recipientAddress, string subject, string message);
+
+		/// <summary>
+		///     Sends a plain-text email to the <paramref name="recipientAddress"/> as an asynchronous operation.
+		/// </summary>
+		/// <param name="recipientAddress">
+		///     The email address of the recipient.
+		/// </param>
+		/// <param name="subject">
+		///     The subject of the email.
+		/// </param>
+		/// <param name="message">
+		///     The message to send.
+		/// </param>
+		/// <param name="cancellationToken">
+		///		The <see cref="CancellationToken"/> to observe.
+		/// </param>
+		/// <returns>
+		///     The <see cref="Task"/> representing the asynchronous operation.
+		/// </returns>
+		Task SendEmailAsync(
+		    string recipientAddress,
+		    string subject,
+		    string message,
+		    CancellationToken cancellationToken = default);
+		
+		/// <summary>
+		///     Sends a plain-text email to the <paramref name="recipientAddress"/> as an asynchronous operation.
+		/// </summary>
+		/// <param name="fromAddress">
+		///		The <see cref="MailAddress"/> to send the email from.
+		/// </param>
+		/// <param name="recipientAddress">
+		///     The email address of the recipient.
+		/// </param>
+		/// <param name="subject">
+		///     The subject of the email.
+		/// </param>
+		/// <param name="message">
+		///     The message to send.
+		/// </param>
+		/// <param name="cancellationToken">
+		///		The <see cref="CancellationToken"/> to observe.
+		/// </param>
+		/// <returns>
+		///     The <see cref="Task"/> representing the asynchronous operation.
+		/// </returns>
+		Task SendEmailAsync(
+			MailAddress fromAddress,
+			string recipientAddress,
+			string subject,
+			string message,
+			CancellationToken cancellationToken = default);
+	}
+}
